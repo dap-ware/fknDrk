334223ad4ad9f06e55d7d1480ed3cec0ad1d7b57
Dap <129771741+dap-ware@users.noreply.github.com>
2023-04-24 15:04:55 -0400

    Update fknDrk.py

--- fknDrk.py
+++ fknDrk.py
@@ -11,14 +11,13 @@ import requests
 from rich.console import Console
 from rich.progress import Progress
 
+
 # Add a lock for printing in multi-threaded environment
 print_lock = threading.Lock()
 console = Console()
 
-
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
-
 
 def print_banner():
     clear_screen()
@@ -26,23 +25,20 @@ def print_banner():
         banner = f.read().strip()
         console.print(banner, style="bold red")
 
-
 def get_proxies():
     proxies = []
-    if os.path.exists("proxies.txt"):
-        with open("proxies.txt", "r") as f:
-            proxies = f.read().split("\n")
-    else:
+    if not os.path.exists("proxies.txt"):
         url = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all&limit=5000"
         proxies = requests.get(url).text.split("\n")
         with open("proxies.txt", "w") as f:
             f.write("\n".join(proxies))
+    else:
+        with open("proxies.txt", "r") as f:
+            proxies = f.read().split("\n")
     return proxies
-
 
 # Use a session to reuse connections
 session = requests.Session()
-
 
 def test_proxy(proxy, user_agent, verbose):
     test_url = "https://bing.com"
@@ -61,11 +57,10 @@ def test_proxy(proxy, user_agent, verbose):
                 console.print(f"Error with proxy {proxy}: {e}", style="bold red")
     return False
 
-
 def filter_working_proxies(proxies, user_agents, verbose):
     working_proxies = []
     user_agent = random.choice(user_agents)
-
+    
     with ThreadPoolExecutor(max_workers=100) as executor:
         futures_to_proxies = {executor.submit(test_proxy, proxy, user_agent, verbose): proxy for proxy in proxies}
 
@@ -76,7 +71,7 @@ def filter_working_proxies(proxies, user_agents, verbose):
                     if verbose:
                         with print_lock:
                             console.print(f"Good proxy found: {futures_to_proxies[future]}", style="green")
-
+    
     return working_proxies
 
 
@@ -89,7 +84,6 @@ def google_search(query, user_agent, proxy):
     return [result["href"] for result in soup.select(".yuRUbf a")]
 
 def try_search_dork(dork, proxy, user_agent, num_results):
-    results = None
     try:
         results = google_search(dork, user_agent, proxy)
         if results:
@@ -104,7 +98,7 @@ def try_search_dork(dork, proxy, user_agent, num_results):
         with print_lock:
             if verbose:
                 console.print(f"Error with proxy: {proxy}", style="bold yellow")
-        return results
+        return None
 
 
 def search_dork(dork, proxies, user_agents, verbose, num_results, threads=50, max_retries=3, backoff_factor=1.0):
@@ -121,10 +115,10 @@ def search_dork(dork, proxies, user_agents, verbose, num_results, threads=50, ma
         retries += 1
         time.sleep(backoff_factor * (2 ** (retries - 1)))
 
+
 def get_user_agents():
     with open("useragents.txt", "r") as f:
         return f.read().split("\n")
-
 
 def main():
     print_banner()
@@ -140,10 +134,14 @@ def main():
 
     user_agents = get_user_agents()
 
-    proxies = []
-    if os.path.exists("proxies.txt"):
-        proxies = get_proxies()
-        working_proxies = filter_working_proxies(proxies, user_agents, args.verbose)
+    if not os.path.exists("proxies.txt"):
+        with console.status("[cyan]Downloading proxies...") as status:
+            proxies = get_proxies()
+    else:
+        with console.status("[cyan]Loading proxies...") as status:
+            proxies = get_proxies()
+
+    proxies = filter_working_proxies(get_proxies(), user_agents, args.verbose)
 
     if not os.path.exists("results"):
         os.makedirs("results")
@@ -155,7 +153,7 @@ def main():
 
     saved_dorks = set()  # to keep track of dorks that have already saved results
     with ThreadPoolExecutor(max_workers=args.threads) as executor:
-        futures = {executor.submit(search_dork, dork, working_proxies, user_agents, args.verbose, args.numResults): dork for dork in dorks}
+        futures = {executor.submit(search_dork, dork, proxies, user_agents, args.verbose, args.numResults): dork for dork in dorks}
         with Progress() as progress:
             task_id = progress.add_task("[cyan] Searching dorks...", total=len(dorks))
             for future in as_completed(futures):
